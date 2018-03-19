9c8784c2f6137cf5a4e9f073f6821fa7138c27d7
root <root@vm.4ub5rs2snenelpdn2elbxifwtf.bx.internal.cloudapp.net>
2018-03-19 21:36:31 +0000

    commit so i can merge

--- get_synonyms.py
+++ get_synonyms.py
@@ -1,5 +1,6 @@
 import re
 import pickle
+import os
 def get_synonyms(filename): #This is meant to be used on "English Synonyms and Antonyms" by James Champlin Fernald
     f = open(filename, "r")
     lines = f.readlines()[460:]
@@ -41,7 +42,7 @@ def rahmGetSyn():
 
     raw_syn_header = re.findall('(?<=\*).*?(?=Synonyms:)', syn, re.DOTALL)
     syn_header = [i.replace('*', "").replace('.', "").strip() for i in raw_syn_header]
-
+    print(syn_body)
     syn_dict = {}
     for i in range(len(syn_header)):
         syn_dict[syn_header[i]] = syn_body[i]
@@ -53,9 +54,41 @@ def rahmGetSyn():
     f.close()
 
 
+def find_common_context(syn_set, five_grams):
+	print("Bla")
+
+def five_grams_read():
+    """im deciding to do all the counting and file opening in one function, so that we can one by one open the files. Otherwise i think the string/list of all lines from all ngrams would crash something """
+	
+    # import synonyms
+    syn_file = open("syn_dict.pickle", 'rb')
+    syn_dict = pickle.load(syn_file)
+    syn_file.close()
+
+    # dict of each header synonym, where the value will be a set of contexts
+    context_dict = {}
+    # initialize with syn_dict keys
+    for key in syn_dict:
+        context_dict[key] = set([])
+
+
+
+	# changes current working directory to the previos directory, where the 5gram data is
+	os.chdir("/./../downloads/google_ngrams/5/")
+	# iterate over each file in the 5gram directory, with checking if it is a 5 gram file.
+	for fname in os.listdir():
+		if fname[:7] != "google":
+			continue
+        curr_file = open(fname, 'r')
+        data = curr_file.readlines()
+        curr_file.close()
+
+        
+
 
 
 if __name__ == "__main__":
+    #five_grams_read()
     rahmGetSyn()
-    #print(get_sgynonyms("test.txt"))
+    #print(get_synonyms("test.txt"))
 
